da6b9ef467fd004d10eb34c697eba5effec2f960
Mao Zhang <mao.zhang233@gmail.com>
2023-01-23 23:53:57 -0600

    add matrix to utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -19,6 +19,7 @@
 #pragma clang diagnostic ignored "-Wnested-anon-types"
 #endif
 
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
@@ -41,6 +42,7 @@ struct Vec2 {
   inline Vec2<T> operator+(const Vec2<T>& v) const;
   inline Vec2<T> operator-(const Vec2<T>& v) const;
   inline Vec2<T> operator*(float f) const;
+  inline T operator[](int index) const;
 
   template <typename>
   friend std::ostream& operator<<(std::ostream& out, Vec2<T>& v);
@@ -65,6 +67,13 @@ Vec2<T> Vec2<T>::operator-(const Vec2<T>& v) const {
 template <typename T>
 Vec2<T> Vec2<T>::operator*(float f) const {
   return Vec2<T>(x * f, y * f);
+}
+
+template <typename T>
+T Vec2<T>::operator[](int index) const {
+  assert(index >= 0 && index < 2);
+
+  return this->raw[index];
 }
 
 template <typename T>
@@ -97,6 +106,7 @@ struct Vec3 {
   inline Vec3<T> operator+(const Vec3<T>& v) const;
   inline Vec3<T> operator-(const Vec3<T>& v) const;
   inline Vec3<T> operator*(float f) const;
+  inline T operator[](int index) const;
   // Dot product
   inline T operator*(const Vec3<T>& v) const;
 
@@ -131,6 +141,13 @@ Vec3<T> Vec3<T>::operator-(const Vec3<T>& v) const {
 }
 
 template <typename T>
+T Vec3<T>::operator[](int index) const {
+  assert(index >= 0 && index < 3);
+
+  return this->raw[index];
+}
+
+template <typename T>
 Vec3<T> Vec3<T>::operator*(float f) const {
   return Vec3<T>(x * f, y * f, z * f);
 }
@@ -161,6 +178,155 @@ std::ostream& operator<<(std::ostream& out, Vec3<T>& v) {
 
 typedef Vec3<int> Vec3i;
 typedef Vec3<float> Vec3f;
+
+template <int M, int N>
+struct Mat {
+  std::vector<std::vector<float> > m;
+  int rows, cols;
+
+  Mat<M, N>();
+
+  std::vector<float>& operator[](int index) const;
+  template <int O>
+  Mat<M, N>& operator*(Mat<N, O>& mat) const;
+
+  template <typename>
+  friend std::ostream& operator<<(std::ostream& out, Mat<M, N>& mat);
+
+  Mat<M, N>& Transpose() const;
+  Mat<M, N>& Inverse() const;
+
+  static Mat<M, N> Identity();
+};
+
+template <int M, int N>
+Mat<M, N>::Mat()
+    : rows(M),
+      cols(N),
+      m(std::vector<std::vector<float> >(M, std::vector<float>(N, 0.f))) {}
+
+template <int M, int N>
+std::vector<float>& Mat<M, N>::operator[](int index) const {
+  assert(index >= 0 && index < M);
+
+  return this->m[index];
+}
+
+template <int M, int N>
+template <int O>
+Mat<M, N>& Mat<M, N>::operator*(Mat<N, O>& mat) const {
+  Mat<M, O> product{};
+
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < O; ++j) {
+      for (int k = 0; k < N; ++k) {
+        product[i][j] += this->m[i][k] * mat[k][i];
+      }
+    }
+  }
+
+  return mat;
+}
+
+template <int M, int N>
+std::ostream& operator<<(std::ostream& out, Mat<M, N>& mat) {
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      out << this->m[i][j] << ", ";
+    }
+    out << std::endl;
+  }
+
+  return out;
+}
+
+template <int M, int N>
+Mat<M, N>& Mat<M, N>::Transpose() const {
+  Mat<N, M> mat{};
+
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      mat[j][i] = this->m[i][j];
+    }
+  }
+
+  return mat;
+}
+
+template <int M, int N>
+Mat<M, N>& Mat<M, N>::Inverse() const {
+  assert(M == N);
+
+  Mat<M, N * 2> mat{};
+
+  // Copy original matrix
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N : ++j) {
+      mat[i][j] = this->m[i][j];
+    }
+  }
+  // Add a row with each element is 1
+  for (int i = 0; i < M; ++i) {
+    mat[i][i + N] = 1.f;
+  }
+  // First pass
+  for (int i = 0; i < M - 1; ++i) {
+    // Normalize the first row
+    for (int j = mat.cols - 1; j >= 0; --j) {
+      mat[i][j] /= mat[i][i];
+    }
+
+    for (int k = i + 1; k < M; ++k) {
+      float coefficient = mat[k][i];
+      for (int j = 0; j < mat.cols; ++j) {
+        mat[k][j] -= mat[i][j] * coefficient;
+      }
+    }
+  }
+  // Normalize the last row
+  for (int j = mat.cols - 1; j >= 0; --j) {
+    mat[M - 1][j] /= mat[M - 1][M - 1];
+  }
+  // Second pass
+  for (int i = M - 1; i >= 0; --i) {
+    for (int k = i - 1; k >= 0; --k) {
+      float coefficient = mat[k][i];
+      for (int j = 0; j < mat.cols; ++j) {
+        mat[k][j] -= mat[i][j] * coefficient;
+      }
+    }
+  }
+  // Cut the identity matrix back
+  Mat<M, N> truncated{};
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      truncated[i][j] = mat[i][j + N];
+    }
+  }
+
+  return truncated;
+}
+
+template <int M, int N>
+Mat<M, N> Mat<M, N>::Identity() {
+  assert(M == N);
+
+  Mat<M, N> E{};
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (i == j) {
+        E[i][j] = 1.f;
+      } else {
+        E[i][j] = 0.f;
+      }
+    }
+  }
+
+  return E;
+}
+
+typedef Mat<4, 4> Mat4;
+
 }  // namespace swr
 
 #endif  // SOFTWARE_RENDERER_INCLUDE_UTILS_H_
